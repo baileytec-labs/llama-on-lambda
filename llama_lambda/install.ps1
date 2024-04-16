f186c23af7651c8f0a1f207f03c6e5f900c6ae2c
sean-bailey <seanbailey518@gmail.com>
2024-04-16 16:55:04 -0400

    Universal Compatibility Update

--- llama_lambda/install.ps1
+++ llama_lambda/install.ps1
@@ -1,20 +1,15 @@
-# This script should be run with PowerShell
-
-# Create a Python virtual environment
 python -m venv .venv
-# Activate the virtual environment
-.venv\Scripts\Activate.ps1
-
-# Install dependencies from requirements.txt
+.\.venv\Scripts\Activate.ps1
 pip install -r requirements.txt
 
-# Prompt the user for the model URL
-$modelURL = Read-Host "Please provide the direct link to the model you'd like to use (Default https://huggingface.co/stabilityai/stablelm-2-zephyr-1_6b/resolve/main/stablelm-2-zephyr-1_6b-Q5_K_M.gguf)"
-
-# If no URL is provided, use the default
-if ([string]::IsNullOrEmpty($modelURL)) {
-    $modelURL = "https://huggingface.co/stabilityai/stablelm-2-zephyr-1_6b/resolve/main/stablelm-2-zephyr-1_6b-Q5_K_M.gguf"
+$MODELURL = Read-Host -Prompt "Please provide the direct link to the model you'd like to use (Default https://huggingface.co/stabilityai/stablelm-2-zephyr-1_6b/resolve/main/stablelm-2-zephyr-1_6b-Q5_K_M.gguf)"
+if ($MODELURL -eq "") {
+    $MODELURL = "https://huggingface.co/stabilityai/stablelm-2-zephyr-1_6b/resolve/main/stablelm-2-zephyr-1_6b-Q5_K_M.gguf"
 }
 
-# Deploy using the cdk command, adjusting for correct context parameter usage in PowerShell
-cdk deploy --context modelfile=$modelURL
+$APIKEY = Read-Host -Prompt "Please provide your API key that you want your Lambda LLM Server to require when using (just hit enter if using legacy)"
+if ($APIKEY -eq "") {
+    $APIKEY = "insert_key_here"
+}
+
+cdk deploy -c modelfile=$MODELURL -c apikey=$APIKEY
